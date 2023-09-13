a38e8a16790d762ba8438822dfa68e1b06f0b8bd
scooter7 <scooter.vineburgh@gmail.com>
2023-09-13 14:31:52 -0500

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -3,40 +3,28 @@ import cv2
 import numpy as np
 from deepface import DeepFace
 from docx import Document
+from docx.shared import Inches
 import base64
+import os
 
 st.title("Emotion Analysis using DeepFace")
 image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
+last_analyzed_image = None
+last_dominant_emotion = None
+
 def analyze_image(image_file):
+    global last_analyzed_image, last_dominant_emotion
     image_data = np.frombuffer(image_file.read(), np.uint8)
-    if len(image_data) == 0:
-        st.write("Could not read image data.")
-        return
-    
     image = cv2.imdecode(image_data, -1)
-    if image is None:
-        st.write("Could not decode image.")
-        return
-
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    if image_rgb is None:
-        st.write("Could not convert image to RGB.")
-        return
-
     predictions = DeepFace.analyze(image_rgb, actions=['emotion'])
-
-    if not isinstance(predictions, dict):
-        st.write("DeepFace.analyze did not return a dictionary as expected.")
-        st.write("Returned value:", predictions)
-        return
-
-    if 'dominant_emotion' not in predictions:
-        st.write("Could not find 'dominant_emotion' in DeepFace.analyze output.")
-        st.write("DeepFace.analyze output:", predictions)
-        return
-
-    st.write("Emotion:", predictions['dominant_emotion'])
+    if isinstance(predictions, dict) and 'dominant_emotion' in predictions:
+        last_dominant_emotion = predictions['dominant_emotion']
+        last_analyzed_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        st.write("Emotion:", last_dominant_emotion)
+    else:
+        st.write("Could not determine emotion.")
 
 if image_file is not None:
     st.image(image_file, caption="Uploaded Image.", use_column_width=True)
@@ -45,8 +33,15 @@ if image_file is not None:
     analyze_image(image_file)
 
 def generate_word_file():
+    global last_analyzed_image, last_dominant_emotion
     doc = Document()
     doc.add_heading('Emotion Analysis Report', 0)
+    if last_analyzed_image is not None and last_dominant_emotion is not None:
+        image_path = 'temp_image.jpg'
+        cv2.imwrite(image_path, cv2.cvtColor(last_analyzed_image, cv2.COLOR_RGB2BGR))
+        doc.add_picture(image_path, width=Inches(2.0))
+        doc.add_paragraph(f'Dominant Emotion: {last_dominant_emotion}')
+        os.remove(image_path)
     doc.save('report.docx')
     with open('report.docx', 'rb') as f:
         bytes = f.read()
