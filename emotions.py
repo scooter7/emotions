55ae425e5f75803834b84b066ae1e9486ac348e1
scooter7 <scooter.vineburgh@gmail.com>
2023-09-13 11:16:21 -0500

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -1,28 +1,47 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from PIL import Image
+import cv2 as cv2  # Importing opencv-python-headless and aliasing it as cv2
 import numpy as np
+from deepface import DeepFace
 from docx import Document
 
-st.title("Emotion Analysis using FER")
+st.title("Emotion Analysis using DeepFace")
 
 image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 def analyze_image(image_file):
-    image = Image.open(image_file)
-    image = np.array(image)
-    detector = FER()
-    emotions = detector.detect_emotions(image)
-    if len(emotions) > 0:
-        st.write("Emotion:", emotions[0]["emotions"])
-    else:
-        st.write("No face detected.")
+    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), -1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    predictions = DeepFace.analyze(image, actions=['emotion'])
+    st.write("Emotion:", predictions['dominant_emotion'])
 
 if image_file is not None:
     st.image(image_file, caption="Uploaded Image.", use_column_width=True)
     st.write("")
     st.write("Classifying...")
     analyze_image(image_file)
+
+video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mkv"])
+
+def analyze_video(video_file):
+    video_bytes = video_file.read()
+    video_path = "uploaded_video.mp4"
+    with open(video_path, "wb") as f:
+        f.write(video_bytes)
+
+    cap = cv2.VideoCapture(video_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        predictions = DeepFace.analyze(frame, actions=['emotion'])
+        st.write("Emotion:", predictions['dominant_emotion'])
+    cap.release()
+
+if video_file is not None:
+    st.video(video_file)
+    st.write("")
+    st.write("Classifying...")
+    analyze_video(video_file)
 
 def generate_word_file():
     doc = Document()
